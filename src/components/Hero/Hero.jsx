97a4a9282025dca9a28821e7bc5a7e6e7320cbb3
srasilva1910 <s.r.a.silva.1910@gmail.com>
2026-06-14 16:02:39 -0600

    Hero and about photo update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Hero.css";
-import raul from "../../assets/images/raulramirez.jpg";
+import raul from "../../assets/images/raulramirez.jpeg";
 
 const Hero = () => {
   return (
